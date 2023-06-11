867acebdfc4eed24ee436781cafc93ad929964a5
Carlos1507 <ayala.carlos@pucp.edu.pe>
2023-06-11 15:27:27 -0500

    login providers configurado

--- app/src/main/java/com/example/lab5_iot/Fragments/LoginFragment.java
+++ app/src/main/java/com/example/lab5_iot/Fragments/LoginFragment.java
@@ -1,17 +1,29 @@
 package com.example.lab5_iot.Fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.lab5_iot.DTOs.User;
+import com.example.lab5_iot.R;
 import com.example.lab5_iot.databinding.FragmentLoginBinding;
+import com.firebase.ui.auth.AuthMethodPickerLayout;
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.auth.api.Auth;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
 public class LoginFragment extends Fragment {
     FragmentLoginBinding binding;
@@ -27,6 +39,18 @@ public class LoginFragment extends Fragment {
         super.onCreate(savedInstanceState);
         firebaseDatabase = FirebaseDatabase.getInstance();
         DatabaseReference databaseReference = firebaseDatabase.getReference();
-
+        User user = new User();
+        user.setNombre("Carlos");
+        user.setContraseniaa("1234");
+        user.setTelefono("987654321");
+        databaseReference.child("users").push().setValue(user)
+                .addOnSuccessListener(aVoid->{
+                    Log.d("msg", "Data guardada exitosamente");
+                })
+                .addOnFailureListener(e -> {
+                    Log.d("msg", e.getMessage());
+                });
     }
+
+
 }
--- app/src/main/java/com/example/lab5_iot/MainActivity.java
+++ app/src/main/java/com/example/lab5_iot/MainActivity.java
@@ -1,11 +1,24 @@
 package com.example.lab5_iot;
 
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.example.lab5_iot.DTOs.User;
 import com.example.lab5_iot.databinding.ActivityMainBinding;
+import com.firebase.ui.auth.AuthMethodPickerLayout;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
@@ -14,5 +27,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        AuthMethodPickerLayout customLayout = new AuthMethodPickerLayout.Builder(R.layout.fragment_login)
+                .setEmailButtonId(R.id.ingresar)
+                .setGoogleButtonId(R.id.loginGoogle)
+                .setFacebookButtonId(R.id.loginFacebook)
+                .build();
+        Intent intent = AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAuthMethodPickerLayout(customLayout)
+                .setAvailableProviders(Arrays.asList(
+                        new AuthUI.IdpConfig.EmailBuilder().build(),
+                        new AuthUI.IdpConfig.GoogleBuilder().build(),
+                        new AuthUI.IdpConfig.FacebookBuilder().build()
+                )).build();
+        signInLauncher.launch(intent);
     }
+    ActivityResultLauncher<Intent> signInLauncher = registerForActivityResult(
+            new FirebaseAuthUIActivityResultContract(),
+            result -> {
+                if (result.getResultCode() == RESULT_OK){
+                    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                    Log.d("msg-tst", "firebase UID: "+user.getUid());
+                    Log.d("msg-tst", "username"+ user.getEmail());
+                }else{
+                    Log.e("mgs-tst","Canceló el flujo");
+                }
+            }
+    );
 }
