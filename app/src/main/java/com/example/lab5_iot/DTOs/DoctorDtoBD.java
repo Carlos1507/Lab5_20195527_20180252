ba4167220442dde707d3c96960917212dc1e52d6
Carlos1507 <ayala.carlos@pucp.edu.pe>
2023-06-11 19:25:40 -0500

    dtos

---
+++ app/src/main/java/com/example/lab5_iot/DTOs/DoctorDtoBD.java
@@ -0,0 +1,121 @@
+package com.example.lab5_iot.DTOs;
+
+public class DoctorDtoBD {
+    private String nombre;
+    private String apellido;
+    private String fotoDoctor;
+    private String genero;
+    private String correo;
+    private float costo;
+    private String pais;
+    private String estado;
+    private String ciudad;
+    private int edad;
+    private String telefono;
+    private String nacionalidad;
+    private String username;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getFotoDoctor() {
+        return fotoDoctor;
+    }
+
+    public void setFotoDoctor(String fotoDoctor) {
+        this.fotoDoctor = fotoDoctor;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public float getCosto() {
+        return costo;
+    }
+
+    public void setCosto(float costo) {
+        this.costo = costo;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getNacionalidad() {
+        return nacionalidad;
+    }
+
+    public void setNacionalidad(String nacionalidad) {
+        this.nacionalidad = nacionalidad;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
